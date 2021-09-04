cb325aa5f840aa813b864305baaa4e587f9c69cf
navyarani-k <navyarani.k@etvbharat.com>
2021-09-04 14:57:26 +0530

    hdhf

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = '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';
+}
